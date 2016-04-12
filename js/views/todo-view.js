7bc99f7e60780b986fc82cc8655e07047345e074
Richard Strutt <richardastrutt@gmail.com>
2016-04-12 15:54:25 +0800

    added a priority button

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -19,6 +19,7 @@ var app = app || {};
 		events: {
 			'click .toggle': 'toggleCompleted',
 			'click .edit-btn': 'edit',
+			'click .priority-btn': 'priority',
 			'dblclick label': 'edit',
 			'click .destroy': 'clear',
 			'keypress .edit': 'updateOnEnter',
@@ -51,6 +52,7 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -69,6 +71,10 @@ var app = app || {};
 		// Toggle the `"completed"` state of the model.
 		toggleCompleted: function () {
 			this.model.toggle();
+		},
+
+		priority: function () {
+			this.model.pToggle();
 		},
 
 		// Switch this view into `"editing"` mode, displaying the input field.
